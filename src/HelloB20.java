f3cae9342e22848bb7a078da5ff4419b0c36a4ca
mfx128 <mehdi.teriaoui@gmail.com>
2020-08-01 11:50:29 -0400

    update Hello B20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -4,6 +4,11 @@ public class HelloB20 {
         System.out.println("Hello B20, it's my pleasure to be with you!");
 
 
+        int num = 2;
+        int num2 = 3;
+
+        System.out.println(num+num2);
+
         //added line
     }
 }
