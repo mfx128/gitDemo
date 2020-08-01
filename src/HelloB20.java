e8991d16ee3d1fecca22031151756c245b32aafc
mfx128 <mehdi.teriaoui@gmail.com>
2020-08-01 11:43:55 -0400

    add HappyB20.java

---
+++ src/HelloB20.java
@@ -0,0 +1,9 @@
+public class HelloB20 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello B20, it's my pleasure to be with you!");
+
+
+        //added line
+    }
+}
